955a08223ba0a3a65dc982ed17b6f7850bcf30a5
nataigna <natacarlton@gmail.com>
2019-01-23 12:03:00 -0800

    Add files via upload

--- pythonclub/club/admin.py
+++ pythonclub/club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Meeting, MeetingMinuted, Resource, Event
 
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(MeetingMinuted)
+admin.site.register(Resource)
+admin.site.register(Event)
--- pythonclub/club/models.py
+++ pythonclub/club/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    agenda=models.TextField()
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField(null=True,blank=True)
+
+    def __str__(self):
+       return self.meetingtitle
+
+    class Meta:
+        db_table='meeting'
+class MeetingMinuted(models.Model):
+    meetingid=models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    attendance=models.ManyToManyField(User)
+    minutestext=models.TextField()
+
+    def __str__(self):
+       return self.meetingid
+
+    class Meta:
+        db_table='meetingminuted'   
+
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    resourseurl=models.URLField()
+    userid=models.ForeignKey(User,on_delete=models.CASCADE)
+    description=models.TextField()
+
+    def __str__(self):
+       return self.resourcename
+    
+    class Meta:
+        db_table='resource'
+
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255) 
+    eventlocation=models.CharField(max_length=255)
+    eventdate=models.DateField()
+    eventtime=models.TimeField()
+    userid=models.CharField(max_length=255)
+
+    def __str__(self):
+       return self.eventtitle
+    
+    class Meta:
+        db_table='event'
+
